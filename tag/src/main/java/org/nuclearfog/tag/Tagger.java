7c105891ff35062a396347464d1ddf059b030b26
nuclearfog <hatespirit666@gmail.com>
2020-01-05 13:32:41 +0100

    added link function, bug fix

--- tag/src/main/java/org/nuclearfog/tag/Tagger.java
+++ tag/src/main/java/org/nuclearfog/tag/Tagger.java
@@ -14,9 +14,10 @@ import java.util.regex.Pattern;
 
 public abstract class Tagger {
 
-    private static final String S_CHARS = "\\^\\!\"\\§\\%\\&\\/\\(\\)\\=\\?\\´\\°\\{\\[\\]\\}\\\\\\`\\+\\-\\*\\'\\~\\.\\,\\;\\:\\<\\>\\|";
-    private static final String PATTERN = "[@#][^@#\\s "+S_CHARS+"]+";
-    private static final Pattern p = Pattern.compile(PATTERN);
+    private static final String LINK_PATTERN_STRING = "[\n\\s]https?://[^\\s]";
+    private static final String TW_PATTERN_STRING = "[\n\\s][@#][^@#\\s\\^\\!\"\\§\\%\\&\\/\\(\\)\\=\\?\\´\\°\\{\\[\\]\\}\\\\\\`\\+\\-\\*\\'\\~\\.\\,\\;\\:\\<\\>\\|]+";
+    private static final Pattern LINK_PATTERN = Pattern.compile(LINK_PATTERN_STRING);
+    private static final Pattern TW_PATTERN = Pattern.compile(TW_PATTERN_STRING);
 
     private static final int MODE = Spanned.SPAN_EXCLUSIVE_EXCLUSIVE;
 
@@ -30,7 +31,8 @@ public abstract class Tagger {
      */
     public static Spannable makeText(final String text, final int color, @NonNull final OnTagClickListener l) {
         SpannableStringBuilder sText = new SpannableStringBuilder(text);
-        Matcher m = p.matcher(text);
+        sText.insert(0, " "); // Add whitespace at begin to match if target string is at beginning
+        Matcher m = TW_PATTERN.matcher(sText);
 
         while (m.find()) {
             final int start = m.start();
@@ -49,6 +51,33 @@ public abstract class Tagger {
                 }
             }, start, end, MODE);
         }
+        sText.delete(0, 1); // Remove first whitespace added at the beginning of this method
+        return sText;
+    }
+
+    public static Spannable makeTextWithLinks(final String text, final int color, @NonNull final OnTagClickListener l) {
+        SpannableStringBuilder sText = new SpannableStringBuilder(makeText(text, color, l));
+        sText.insert(0, " "); // Add whitespace at begin to match if target string is at beginning
+        Matcher m = LINK_PATTERN.matcher(sText);
+
+        while (m.find()) {
+            final int start = m.start();
+            final int end = m.end();
+
+            sText.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(@NonNull View widget) {
+                    l.onClick(text.substring(start, end));
+                }
+
+                @Override
+                public void updateDrawState(@NonNull TextPaint ds) {
+                    ds.setColor(color);
+                    ds.setUnderlineText(false);
+                }
+            }, start, end, MODE);
+        }
+        sText.delete(0, 1); // Remove first whitespace added at the beginning of this method
         return sText;
     }
 
@@ -62,8 +91,8 @@ public abstract class Tagger {
      */
     public static Spannable makeText(String text, int color) {
         SpannableStringBuilder sText = new SpannableStringBuilder(text);
-        Pattern p = Pattern.compile(PATTERN);
-        Matcher m = p.matcher(text);
+        sText.insert(0, " "); // Add whitespace at begin to match if target string is at beginning
+        Matcher m = TW_PATTERN.matcher(sText);
 
         while (m.find()) {
             final int start = m.start();
@@ -72,6 +101,7 @@ public abstract class Tagger {
             ForegroundColorSpan sColor = new ForegroundColorSpan(color);
             sText.setSpan(sColor, start, end, MODE);
         }
+        sText.delete(0, 1); // Remove first whitespace added at the beginning of this method
         return sText;
     }
 
